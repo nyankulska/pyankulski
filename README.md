b37d89bbff8b936cda9a5e0fbbe7ebc9352525c5
Dimitar Kalinkov <dimitar.kalinkov@tbs.tech>
2020-03-19 23:41:59 +0200

    feat: add contact profile static info files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Welcome
+
+Virtual Business Card
---
+++ docs/precache-manifest.e9ac3edad75d35c8e06d00b64d844793.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7e032e42af16a77937b3d108bc6646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb421d2a01792fcd32ea",
+    "url": "/static/css/main.0ee31ef0.chunk.css"
+  },
+  {
+    "revision": "a75eda9ec74cf2be0b83",
+    "url": "/static/js/2.1333edfb.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.1333edfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb421d2a01792fcd32ea",
+    "url": "/static/js/main.dbba0cce.chunk.js"
+  },
+  {
+    "revision": "090312cf7ac90fa643e2",
+    "url": "/static/js/runtime-main.f5fdb611.js"
+  },
+  {
+    "revision": "53c3cd6be32b2e8df4299d50922f12e4",
+    "url": "/static/media/avatar.53c3cd6b.jpg"
+  }
+]);
